8175e5b94ca8bb71917956f2598b7ee2bbd521f4
Ken Ho <ken.ho@hotmail.co.uk>
2016-02-05 04:30:45 +0000

    Create root page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,23 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  function smooth_scroll_to(elem){
+    var offset = 0;
+    offset = $(elem).offset().top - 50;
+    $('html, body').animate({
+        scrollTop: offset
+    }, 1200, 'easeInOutQuad');
+  }
+
+  // Initiate smooth scroll to area based on navigation item title attr
+  $(document).on("click", "#aboutlink, #projectslink, #contactlink", function(e){
+      e.preventDefault();
+      var elem = $(this).attr('href');
+      smooth_scroll_to(elem);
+    });
+})
